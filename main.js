54c6043bd9ce0e0b008bffc53e0f28769938338a
Yoloboss456 <sonicjosh.562@gmail.com>
2017-03-25 16:50:18 -0700

    got latest from master

--- main.js
+++ main.js
@@ -1,42 +1,45 @@
-var answer1 = document.getElementById('img1').innerHTML;
-var answer2 = "Green Day";
-var answer3 = "Eagles";
-var answer4 = "Queen";
-var answer5 = "Journey";
-var answer6 = "Red Hot Chili Peppers";
-var test = document.getElementById('minigame');
 
-function getAnswer(answer1, answer2, answer3, answer4, answer5, answer6) {
-  if (answer1 = "Aerosmith") {
-    console.log("You got it!");
-  }
-  else {
-    console.log("Incorrect");
-  }
-  if (answer2 = "Green Day") {
-    console.log("Yes!");
-  } else {
-    console.log("Nope, Sorry.");
-  }
-  if (answer3 = "Eagles") {
-    console.log("You did it!");
-  } else {
-    console.log("No");
-  }
-  if (answer4 = "Queen") {
-    console.log("Correct!");
-  } else {
-    console.log("Incorrect");
-  }
-  if (answer5 = "Journey") {
-    console.log("Good Job!");
-  } else {
-    console.log("Nope");
-  }
-  if (answer6 = "Red Hot Chili Peppers") {
-    console.log("Great Job!");
-  } else {
-    console.log("Not right.");
-  }
-}
-getAnswer();
+$(document).ready(function() {
+    $('.button-click').click(function() {
+    var answer1 = document.getElementsByClassName("answer1");
+    var answer2 = document.getElementsByClassName("answer2");
+    var answer3 = document.getElementsByClassName("answer3");
+    var answer4 = document.getElementsByClassName('answer4');
+    var answer5 = document.getElementsByClassName('answer5');
+    var answer6 = document.getElementsByClassName('answer6');
+
+        
+    if (answer1 === "Aerosmith") {
+      alert("You got the first one right!");
+    }
+    else {
+      alert("You got the first one wrong");
+    }
+    if (answer2 === "Green Day") {
+      alert("You got the second one right!");
+    } else {
+      alert("You got the second one wrong");
+    }
+    if (answer3 === "The Eagles") {
+      alert("You got the third one right!");
+    } else {
+      alert("You got the third one wrong");
+    }
+    if (answer4 === "Queen") {
+      alert("You got the fourth one right!");
+    } else {
+      alert("You got the fourth one wrong");
+    }
+    if (answer5 === "Journey") {
+      alert("You got the fifth one right!");
+    } else {
+      alert("You got the fifth one wrong");
+    }
+    if (answer6 === "Red Hot Chili Peppers") {
+      alert("You got the sixth one right!");
+    } else {
+      alert("You got the sixth one wrong");
+    }
+  });
+
+});
